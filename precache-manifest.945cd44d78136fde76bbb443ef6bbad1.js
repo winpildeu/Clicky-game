9e07d8b9220765b2f0c125e5563e83e8a75ba799
Winfield H <winterfreshness@gmail.com>
2019-01-17 17:57:54 -0500

    Updates

---
+++ precache-manifest.945cd44d78136fde76bbb443ef6bbad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2fa5eefebf1c6a4a0e5",
+    "url": "/winpildeu/Clicky-game/static/js/runtime~main.b2fa5eef.js"
+  },
+  {
+    "revision": "3ef03219b4c63f48c006",
+    "url": "/winpildeu/Clicky-game/static/js/main.3ef03219.chunk.js"
+  },
+  {
+    "revision": "01a49bde3cfdfae3bb4d",
+    "url": "/winpildeu/Clicky-game/static/js/1.01a49bde.chunk.js"
+  },
+  {
+    "revision": "3ef03219b4c63f48c006",
+    "url": "/winpildeu/Clicky-game/static/css/main.bc99786a.chunk.css"
+  },
+  {
+    "revision": "a9ea9d6b9bcebbcef2befb423862c203",
+    "url": "/winpildeu/Clicky-game/index.html"
+  }
+];
